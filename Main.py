53208dcc90cc1a65ca55bad69b4b7c9d508933a5
XxVenom759xX <iblilyquist@gmail.com>
2023-02-03 12:27:19 -0700

    added stage 1 of the main platform

--- Main.py
+++ Main.py
@@ -1,2 +1,27 @@
 #Dark Souls
+#first description
 print("You wake up on a small platform of complex tiles with guard rails made of marble on all sides except north. The world is a yellow-ish red color and the sun is blaring on you. In the distance there is a forrest full of fall leaves and to the north-west you see a large catherdral of red stone. You are wearing blackened leather armour, but you can tell it won't last long.")
+#second room description
+print("You head towards the north and see a staircase going downwards and to the left hanging along the side of a building made of the same red stone. When you reach the bottom of the staircase you see a giant Knight dressed in black iron armour. The walkway ends where the Knight is standing and there is a door to the south that leads into a room.")
+#first room inside a building
+print("The building is tall and has arches for doorways. Inside the room you see another pair of the giant Knights across the room from each other. There is a chest that the two are gaurding. To the West you see an exit to the building")
+#second open platform
+print("You exit the building and step out to another open platform, this one much larger than the last. To the south there is a doorway leading into a building. To the north there is a round building that appears to lead downwards.")
+#down the stairway near the second platform
+print(f"You go down the stairs and see an unlit \x1B[3mbonfire\x1B[0m and, leaning against a wall opposite you, you see a \x1B[3mFirebearer\x1B[0m")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
