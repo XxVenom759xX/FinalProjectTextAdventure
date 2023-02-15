d4aab6cd99ef652ea03e46dd9bc5c29fb0711679
XxVenom759xX <95148489+XxVenom759xX@users.noreply.github.com>
2023-02-15 02:16:44 +0000

    added new prints

--- Main.py
+++ Main.py
@@ -8,11 +8,15 @@ print("The building is tall and has arches for doorways. Inside the room you see
 #second open platform
 print("You exit the building and step out to another open platform, this one much larger than the last. To the south there is a doorway leading into a building. To the north there is a round building that appears to lead downwards.")
 #down the stairway near the second platform
-print(f"You go down the stairs and see an unlit \x1B[3mbonfire\x1B[0m and, leaning against a wall opposite you, you see a \x1B[3mFirebearer\x1B[0m")
-
-
-
-
+print(f"You go down the stairs and see a \x1B[3mbonfire\x1B[0m and, leaning against a wall opposite you, you see a \x1B[3mFirebearer\x1B[0m")
+#the first elevator
+print("You go to the round building and see that when you step inside there is an elevator that leads downwards.")
+#first main second floor platform
+print("There is a curved stairway that leads you to a stone walkway. There are small, spiked gaurd rails lining the path to the elevator that only go up to your ankles. You can see a small ledge around the round elevator entrance and you can see another elecator to the north. To the west there are buttresses that lead to another building with multiple levels inside.")
+#around the first elevator.
+print("You walk around the edge of the rounded elevator walls and you find a \x1B[3chest\x1B[0m")
+#up the buttresses
+print("You find a missing link in the small gaurd rails that allows you to climb the buttress. When you reach the top there is a broken window")
 
 
 
